16db6d702b291a4131af880af97f07fb8f9058a7
Megha1108 <114331789+Megha1108@users.noreply.github.com>
2023-06-08 16:52:27 +0530

    ass6.asm

--- ass6.asm
+++ ass6.asm
@@ -2,21 +2,21 @@
 
 
 %macro print 2
-mov rax,1
-mov rdi,1
-mov rsi,%1
-mov rdx,%2
+mov rax ,1
+mov rdi ,1
+mov rsi ,%1
+mov rdx ,%2
 syscall
 %endmacro
 
 section .data
-m db 10,"ALP to perform BDT without using string related instructions",10
+m db 10,"ALP to perform BDT without using string related INsruction",10
 l equ $-m
 
-m1 db 10, "source block contents before trsansfer:-"
+m1 db 10,"Source block content before transfer:-"
 l1 equ $-m1
 
-m2 db 10, "destination block contents after transfer:-"
+m2 db 10,"destination block content after transfer:-"
 l2 equ $-m2
 
 space db " "
@@ -24,11 +24,10 @@ spacelen equ $-space
 cnt equ 5
 
 srcblk db 10h,20h,30h,40h,50h
-dstblk db 00,00,00,00,00
+dstblk db 00,00,00,00,00,00
 
 section .bss
 ans resb 2
-
 
 section .text
 global _start
@@ -46,7 +45,7 @@ mov rdi,dstblk
 mov rcx,05
 again:
 mov dl,[rsi]
-mov [rdi],dl
+mov[rdi],dl
 inc rsi
 inc rdi
 loop again
@@ -59,12 +58,9 @@ mov rax,60
 mov rdi,0
 syscall
 
-
-
 display:
 mov rbp,cnt
-up:
-mov al,[rsi]
+up:mov al,[rsi]
 push rsi
 call disp_8
 print space,1
@@ -78,17 +74,20 @@ disp_8:
 mov rdi,ans
 mov rcx,02
 up1:
-				rol al,4
-				mov dl,al
-				and dl,0fh
-				add dl,30h
-				cmp dl,39h
-				jbe dispskip1
-				add dl,07h
+
+	rol al,4
+	mov dl,al
+	and dl,0fh
+	add dl,30h
+	cmp dl,39h
+	jbe dispskip1
+	add dl,07h
+	
 dispskip1:
-				mov [rdi],dl
-				inc rdi
-				loop up1
-				print ans,2
-			ret 
-				
+	mov [rdi],dl
+	inc rdi
+	loop up1
+	print ans,2
+	ret
+
+
