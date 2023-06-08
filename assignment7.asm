bb34943cd959b3ee62673323407e2a6ad5556dc0
Megha1108 <114331789+Megha1108@users.noreply.github.com>
2023-06-08 16:51:50 +0530

    assignment7.asm

--- assignment7.asm
+++ assignment7.asm
@@ -1,19 +1,21 @@
+;to perform non overlapped block transfer with string specific instruction
+
 %macro print 2
-mov rax,1
-mov rdi,1
-mov rsi,%1
-mov rdx,%2
+mov rax ,1
+mov rdi ,1
+mov rsi ,%1
+mov rdx ,%2
 syscall
 %endmacro
 
 section .data
-m db 10,"ALP to perform BDT without using string related instructions",10
+m db 10,"ALP to perform BDT with using string related instruction",10
 l equ $-m
 
-m1 db 10, "source block contents before trsansfer:-"
+m1 db 10,"Source block content before transfer:-"
 l1 equ $-m1
 
-m2 db 10, "destination block contents after transfer:-"
+m2 db 10,"destination block content after transfer:-"
 l2 equ $-m2
 
 space db " "
@@ -21,11 +23,10 @@ spacelen equ $-space
 cnt equ 5
 
 srcblk db 10h,20h,30h,40h,50h
-dstblk db 00,00,00,00,00
+dstblk db 00,00,00,00,00,00
 
 section .bss
 ans resb 2
-
 
 section .text
 global _start
@@ -52,12 +53,9 @@ mov rax,60
 mov rdi,0
 syscall
 
-
-
 display:
 mov rbp,cnt
-up:
-mov al,[rsi]
+up:mov al,[rsi]
 push rsi
 call disp_8
 print space,1
@@ -71,17 +69,20 @@ disp_8:
 mov rdi,ans
 mov rcx,02
 up1:
-				rol al,4
-				mov dl,al
-				and dl,0fh
-				add dl,30h
-				cmp dl,39h
-				jbe dispskip1
-				add dl,07h
+
+	rol al,4
+	mov dl,al
+	and dl,0fh
+	add dl,30h
+	cmp dl,39h
+	jbe dispskip1
+	add dl,07h
+	
 dispskip1:
-				mov [rdi],dl
-				inc rdi
-				loop up1
-				print ans,2
-			ret 
-				
+	mov [rdi],dl
+	inc rdi
+	loop up1
+	print ans,2
+	ret
+
+
